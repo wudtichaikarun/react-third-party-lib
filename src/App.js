b7abf6027e378c5b89bab8d58689bee8223aadd6
Romantic <595159090004@dpu.ac.th>
2018-02-13 03:16:11 +0700

    Third Party Lib Integration: 1.Introduction

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import uuidv4 from 'uuid-v4'
+
+const Box = ({ item }) => (
+  <div className='card m-2'>
+    <div className='m-2' >{item}</div>
+  </div>
+)
+
+class App extends Component {
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    this.addItems()
+  }
+
+  addItems() {
+    this.setState(
+      ({ items }) => ({
+        items: [
+          ...items,
+          ...Array.from({ length: 30 }).map(() => uuidv4())
+        ]
+      })
+    )
+  }
+
+  render() {
+    return (
+      <div className="card-columns m2">
+         {
+           this.state.items.map(
+             item => <Box key={item} item={item} />
+           )
+         }
+      </div>
+    );
+  }
+}
+
+export default App;
